48bc1acfd2bf8de4ee250052bb516e2e789f6d5d
ahmed5768 <101082079+ahmed5768@users.noreply.github.com>
2024-02-17 03:43:44 +0500

    done

---
+++ src/app.js
@@ -0,0 +1,17 @@
+// variable declare
+var var1, var2, var3
+
+// Ieggal variable
+var myVar
+var $var
+var _var
+var camelCase
+var _my_name
+
+// illegal variable
+var 1var
+var Rose 
+var @myname
+var my name
+var month&year
+var birth-year  
